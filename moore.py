ebe754258b652ad4fdee72753e3859c1c44e8203
Richard Potthoff <richard.potthoff@richardpotthoff.com>
2016-08-25 21:57:23 +0000

    add moore_wav

--- moore.py
+++ moore.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+from __future__ import absolute_import
+from __future__ import print_function
 from pylab import *
 import matplotlib
+from six.moves import range
 def show_plot():
   axis('off')
   axes().set_aspect('equal', 'datalim')	
@@ -35,7 +38,7 @@ z4=moore(3)
 z6=moore(6)
 n=8
 zu=concatenate([[z4[i-1]*(n-k)+z4[i]*k for k in range(1,n+1)] for i in range(len(z4))])/n
-zu=roll(zu,-n/2+1)
+zu=roll(zu,-n//2+1)
 #zu=z6
 zuf=fft(zu)
 z1f=copy(zuf)
@@ -44,10 +47,10 @@ z1f[nf:len(z1f)-1-nf]=0
 z1i=ifft(z1f)
 plot(z4.real,z4.imag)
 show_plot()
-print "area=%f, length=%f"%(area_length(z4))
+print("area=%f, length=%f"%(area_length(z4)))
 plot(z1i.real,z1i.imag)
 show_plot()
-print "area=%f, length=%f"%(area_length(z1i))
+print("area=%f, length=%f"%(area_length(z1i)))
 plot(z1i[7:128].real,z1i[7:128].imag)
 plot(z4[:16].real,z4[:16].imag)
 zj=z1i
@@ -56,7 +59,7 @@ for i in range(3):
   zj=0.5*(roll(zj,-1)+roll(zj,1))
 plot(zj[7:128].real,zj[7:128].imag)
 #zk=ifft([zuf[i]*(0.5*(1+cos(pi*i*2/len(zu))))**(exp(2)) for i in range(len(zu))])
-rcos=(0.5*(1+cos(pi*2*array(range(len(zu)))/len(zu))))
+rcos=(0.5*(1+cos(pi*2*array(list(range(len(zu))))/len(zu))))
 zk=ifft(zuf*rcos**exp(2))
 plot(zk[7:128].real,zk[7:128].imag)
 show_plot()
@@ -64,12 +67,12 @@ plot(zj.real,zj.imag)
 plot(zk.real,zk.imag)
 show_plot()
 
-print "area=%f, length=%f"%(area_length(zj))
+print("area=%f, length=%f"%(area_length(zj)))
 zjf=fft(zj)
-hj=array([zjf[i]/zuf[i] if zuf[i]<>(0.0+0.0j) else 0.0+0.0j for i in range(len(zu))])
+hj=array([zjf[i]/zuf[i] if zuf[i]!=(0.0+0.0j) else 0.0+0.0j for i in range(len(zu))])
 zc=exp(1j*2*pi*arange(len(zu))/len(zu))*sqrt(1.9/pi)
 zcf=fft(zc)
-print"circle fourier=",zcf[:5],zcf[-5:]
+print("circle fourier=",zcf[:5],zcf[-5:])
 plot (abs(hj))
 #plot (angle(hj))
 for x in (-2,0,1.85,4,8):
@@ -83,7 +86,7 @@ scale=4.0
 z=arange(0,10.5*scale,scale)
 #z[-1]=79
 #z[-1]=9.4*scale
-print z[-1]
+print(z[-1])
 xa=array([sqrt(2.0/area_length(ifft(zuf*rcos**exp(y1/scale+2)))[0] )for y1 in z])
 #plot(xa,z)
 x=array([blend(ifft(zuf*rcos**exp(y1/scale+2)),zc,(0.5*(1+cos(pi*(1-y1/z[-1]))))**6)[[k,len(zu)/4,0]] for y1 in z])
@@ -99,7 +102,8 @@ for y1 in z:
   plot(x.real,x.imag)
 show_plot()
 test=fft(x)
-print "last fft:",test[:5],test[-5:]
-print "nzu=%d, nz=%d, nz6=%d"%(len(zu),len(z4),len(z6))
+print("last fft:",test[:5],test[-5:])
+print("nzu=%d, nz=%d, nz6=%d"%(len(zu),len(z4),len(z6)))
 plot(abs(roll(x,-1)-x))
 show()
+
---
+++ moore_wav.py
@@ -0,0 +1,60 @@
+import wave
+import random
+import struct
+import datetime
+import os
+import sndhdr
+import sound
+#from math import pi,sin,cos
+from pylab import *
+def  hilbert(n):
+  a=1+1j
+  b=conj(a)
+  z=array([0])
+  for k in range(n):
+    w=1j*conj(z)
+    z=concatenate([w-a,z-b,z+a,b-w])/2.0
+  return z
+def moore(n):
+  z=-conj(hilbert(n))
+  w=concatenate([z+1+1j,z-1+1j])
+  return concatenate([w,-1.0*w])/2.0
+
+m=400
+noise_output = wave.open('noise2.wav', 'wb')
+#noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))#for wave
+noise_output.setparams((2, 2, 44100, 4096*m,'NONE', b'not compressed'))
+d1 = datetime.datetime.now()
+values = []
+maxint=2**15-1
+f=pi*2.0/44100*880
+pi4=pi/4
+z4=moore(3)
+n=4
+zu=concatenate([[z4[i-1]*(n-k)+z4[i]*k for k in range(1,n+1)] for i in range(len(z4))])/n
+zu=roll(zu,-n//2+1)
+print (len(zu))
+zuf=fft(zu)
+rcos=(0.5*(1+cos(pi*2*array(list(range(len(zu))))/len(zu))))
+yf=zuf*rcos**exp(1)
+y=ifft(yf)
+value_str=b''.join([struct.pack('h',int(maxint*x)) for z in y for x in (real(z),imag(z))])
+print(len(value_str))
+noise_output.writeframes(value_str*m)
+
+d2 = datetime.datetime.now()
+print ((d2 - d1), "(time for writing frames)")
+
+noise_output.close()
+
+d3 = datetime.datetime.now()
+print( (d3 - d2), "(time for closing the file)")
+# Simple demo of playing a looping sound using the (currently undocumented) sound.Player class
+noise_input = sound.Player('noise2.wav')
+#noise_input = open((os.path.expanduser(os.path.join(os.path.dirname(os.__file__), "../Media/Sounds/game/Beep.caf"))),'rb')
+print(noise_input.duration)
+noise_input.number_of_loops=0
+noise_input.play()
+import photos
+#img=photos.capture_image()
+#img.save('osci.jpg')
