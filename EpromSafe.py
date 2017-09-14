042dc14d0e631750f93ccb3b26d058a3b4615a14
Richard Potthoff <richard.potthoff@richardpotthoff.com>
2017-09-14 00:33:56 +0000

    invert logic levels

--- EpromSafe.py
+++ EpromSafe.py
@@ -44,7 +44,9 @@ def simulateEprom(address,actions,addressInversionMask=0x0000,dataInversionMask=
   phase=grayToInt((address>>7)&0x3)
   newstate=actions[state][phase]
   return (intToGray(newstate&0x7F)|(newstate&0x80))^dataInversionMask
-  
+
+addressInversionMask=(1<<9)-1 
+dataInversionMask=(1<<8)-1
 combination=[-2,30,-23,10,-2]
 i=0
 p=0
@@ -59,12 +61,12 @@ for j in range(i,64):
 for i in range(64,128):
   actions[i]=rotate_left([(i+1)&127,i,i,i if i==127 else i+3 if i==64 else i-1],i%4)
 for i,a in enumerate(actions): print(i,a)  
-Eprom=[simulateEprom(address,actions) for address in range(1<<9)]
+Eprom=[simulateEprom(address,actions,addressInversionMask,dataInversionMask) for address in range(1<<9)]
 def add_statusbits(i):
   i%=0x80
   return 0x80 if i==0 else i
 actions=[rotate_left(map(add_statusbits,[i+1, i,i,i-1]),(i+1)%4) for i in range(0x80)]
-CounterEprom=[simulateEprom(address,actions) for address in range(1<<9)]
+CounterEprom=[simulateEprom(address,actions,addressInversionMask,dataInversionMask) for address in range(1<<9)]
 def makeOscEprom(nbits):
   n=1<<nbits
   return [intToGray((grayToInt(i)+1)%n) for i in range(n)]  
@@ -79,7 +81,7 @@ for k in [2]+combination+[12]:
     phase+=1 if k>0 else -1
     for i in range(2):
 #      newstate=actions[state&0x7F][phase%4]
-      newstate=Eprom[state&0x7F|(intToGray((-phase)%4)<<7)]
+      newstate=Eprom[(state&0x7F|(intToGray((-phase)%4)<<7))^addressInversionMask]^dataInversionMask
 #      print('Old State: %3d, direction %s, phase: %3d, New State:%3d'%(state,'L'if k>0 else 'R',phase,newstate))
       print('Old State: %3d, direction %s, phase: %3d, New State:%3d'%(grayToInt(state&0x7F)|(state&0x80),'L'if k>0 else 'R',(phase+64)%32,grayToInt(newstate&0x7f)|(newstate&0x80)))
       oldphase=phase
