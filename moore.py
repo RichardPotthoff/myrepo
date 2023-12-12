868217f8d5b888d94be196c70b60aaff2c68e362
Richard Potthoff <RichardPotthoff@users.noreply.github.com>
2023-12-12 18:49:41 -0500

    refine filter function

--- moore.py
+++ moore.py
@@ -35,6 +35,8 @@ def area_length(x):
 def blend(x,y,alpha):
   return alpha*y+(1.0-alpha)*x
 z4=moore(3)
+a4,l4=area_length(z4)
+r4=(a4/pi)**0.5
 z6=moore(6)
 n=8
 zu=concatenate([[z4[i-1]*(n-k)+z4[i]*k for k in range(1,n+1)] for i in range(len(z4))])/n
@@ -51,17 +53,18 @@ print("area=%f, length=%f"%(area_length(z4)))
 plot(z1i.real,z1i.imag)
 show_plot()
 print("area=%f, length=%f"%(area_length(z1i)))
-plot(z1i[7:128].real,z1i[7:128].imag)
-plot(z4[:16].real,z4[:16].imag)
+plot(z1i[7:128].real,z1i[7:128].imag,label='z1i')
+plot(z4[:16].real,z4[:16].imag,label='z4')
 zj=z1i
 #zj=zu
 for i in range(3):
   zj=0.5*(roll(zj,-1)+roll(zj,1))
-plot(zj[7:128].real,zj[7:128].imag)
+plot(zj[7:128].real,zj[7:128].imag,label='zj')
 #zk=ifft([zuf[i]*(0.5*(1+cos(pi*i*2/len(zu))))**(exp(2)) for i in range(len(zu))])
 rcos=(0.5*(1+cos(pi*2*array(list(range(len(zu))))/len(zu))))
 zk=ifft(zuf*rcos**exp(2))
-plot(zk[7:128].real,zk[7:128].imag)
+plot(zk[7:128].real,zk[7:128].imag,label='zk')
+plt.legend()
 show_plot()
 plot(zj.real,zj.imag)
 plot(zk.real,zk.imag)
@@ -83,23 +86,34 @@ zuabs=abs(zu)
 k=argmax(zuabs)
 #print "max=%f,index=%d,max(zuabs)=%f"%(zuabs[k],k,max(zuabs))
 scale=4.0
-z=arange(0,10.5*scale,scale)
+z=arange(0,13.5*scale,scale)
+nz=0.5
 #z[-1]=79
 #z[-1]=9.4*scale
 print(z[-1])
-xa=array([sqrt(2.0/area_length(ifft(zuf*rcos**exp(y1/scale+2)))[0] )for y1 in z])
+xa=array([sqrt(2.0/area_length(ifft(zuf*rcos**exp(10*(y1/(10*scale)+0.1)**nz+2)))[0] )for y1 in z])
 #plot(xa,z)
-x=array([blend(ifft(zuf*rcos**exp(y1/scale+2)),zc,(0.5*(1+cos(pi*(1-y1/z[-1]))))**6)[[k,len(zu)//4,0]] for y1 in z])
+rzu=r4*len(zuf)
+zuf[1]-=rzu
+rzu_=zeros(zuf.shape)
+rzu_[1]=rzu
+x=array([(ifft(zuf*rcos**exp(10*(y1/(10*scale)+0.1)**nz+2)+rzu_))[[k,len(zu)//4,0]] for y1 in z])
+#x=array([blend(ifft(zuf*rcos**exp(10*(y1/(10*scale)+0.1)**nz+2)),zc,(0.5*(1+cos(pi*(1-y1/z[-1]))))**6)[[k,len(zu)//4,0]] for y1 in z])
 plot(abs(x)*20.0,z)
 axes().set_aspect('equal', 'datalim')	
 show()
 close()
+
 for y1 in z:  
-  xf=zuf*rcos**exp(y1/scale+2)
+  xf=zuf*rcos**exp(10*(y1/(10*scale+0.1))**nz+2)
+  xf[1]+=rzu
 #  if y1==z[-1]:xf=zcf
-  x=blend(ifft(xf),zc,(0.5*(1+cos(pi*(1-y1/z[-1]))))**6)
+  
+  x=ifft(xf)
+#  x=blend(ifft(xf),zc,(0.5*(1+cos(pi*(1-y1/z[-1]))))**6)
 #  x=x*sqrt(2.0/area_length(x)[0])
   plot(x.real,x.imag)
+zuf[1]+=rzu
 show_plot()
 test=fft(x)
 print("last fft:",test[:5],test[-5:])
