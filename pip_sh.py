7740ba39ef2f91db7d866a0eee8589a68f145c6d
Richard Potthoff <RichardPotthoff@users.noreply.github.com>
2025-08-05 15:01:58 -0400

    add optional "CLONE_TARGET.py" to tag directory for cloned packages

--- pip_sh.py
+++ pip_sh.py
@@ -6,11 +6,19 @@ try:
   import PIP_TARGET
 except ImportError:
   print('Module "PIP_TARGET.py" missing in "site-packages(user)"!\n\n'
-        'Add an empty file named "TIP_TARGET.py" to "site-packages(user)". \n'
+        'Add an empty file named "PIP_TARGET.py" to "site-packages(user)". \n'
         'This file acts as a tag that marks the target directory for the\n'
         'commands "pip" and "clone".',file=sys.stderr)
   sys.exit(1)
-os.environ['PIP_TARGET']=os.path.dirname(PIP_TARGET.__file__)
+  
+pip_packages_path=os.path.dirname(PIP_TARGET.__file__)
+os.environ['PIP_TARGET']=pip_packages_path
+cloned_packages_path=pip_packages_path
+try:
+  import CLONE_TARGET
+  cloned_packages_path=os.path.dirname(CLONE_TARGET.__file__)
+except ImportError:
+  pass
 
 def clone_selected(src, dst, selected, logging=False):
     src, dst = Path(src), Path(dst)
@@ -26,7 +34,7 @@ def clone_selected(src, dst, selected, logging=False):
             dst_path.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy(src_path, dst_path)
         else:
-            raise FileNotFoundError(f"source file '{name}' not found!")    
+            raise FileNotFoundError(f"source file or directory '{item}' not found!")    
         if logging:print('O.K.')
       except Exception as e:
         if logging:
@@ -48,7 +56,7 @@ def sh_cmd(cmdln):
                         return(_main())
                       except BaseException: 
                         return -1
-    elif 'clone'==cmd: clone_selected(src=os.getcwd(),dst=os.environ['PIP_TARGET'],selected=args,logging=True);return 0
+    elif 'clone'==cmd: clone_selected(src=os.getcwd(),dst=cloned_packages_path,selected=args,logging=True);return 0
     elif 'ls'   ==cmd: print('\n'.join(sorted(os.listdir(*args))));  return 0
     elif 'cd'   ==cmd: os.chdir(*args); return 0
     elif 'quit' ==cmd: sys.exit()
@@ -62,13 +70,16 @@ def sh_cmd(cmdln):
 def sh():
   print(
   'Command List:\n'
-  '  pip: This is the standard python pip command.\n'
+  '  pip: This is the standard Python pip command.\n'
   '       If "pip" is not yet installed, copy "pip_sh.py" to a remote share that\n'
   '       contains an installed "pip" module (e.g. on a shared remote drive) to\n'
-  '       bootstrap the installation of "pip". Running pip from a remote shared \n'
-  '       drive may be slow, so it should be avoided once "pip" is installed locally.\n'
-  'clone: This command allows to copy a list of files/directories from the\n'
-  '       "cwd" (current working directory) to "site-packes(user)" (PIP_TARGET).\n'
+  '       bootstrap the installation of "pip" (using the command: "pip install pip").\n'
+  '       Running "pip" from a remote shared drive may be slow (~30s to install pip),\n'
+  '       so it should be avoided once "pip" is installed locally.\n'
+  'clone: This command copies a list of files/directories from the "cwd" (current\n'
+  '       working directory) to "site-packages(user)" (PIP_TARGET), or to a directory\n'
+  '       with a file "CLONE_TARGET.py" (the directories have to be on the "sys.path"\n'
+  '       search path so Python can find them).\n'
   '       Tip: copy/paste the filenames from the "ls" output to the input line.\n'
   '   ls: List an alphabetically sorted list of files/directories of the "cwd".\n' 
   '   cd: Change the "cwd".\n'
