9c109d18e42319fad7f1aefa73e92f38cba28308
Richard Potthoff <RichardPotthoff@users.noreply.github.com>
2025-08-06 17:17:59 -0400

    use filename or an optional argument to give the shell a name

--- pip_sh.py
+++ pip_sh.py
@@ -1,5 +1,26 @@
 import sys, os, pathlib, shutil, shlex, runpy
-
+sh_name=os.path.splitext(os.path.basename(__file__))[0] if '__file__' in locals() else 'pip_sh'
+def help_text(): return ('\n'
+  'List of Commands:\n\n'
+  '  pip: This is the standard Python pip command.\n'
+  '       If "pip" is not yet installed, copy "pip_sh.py" to a remote share that\n'
+  '       contains an installed "pip" module (e.g. on a shared remote drive) to\n'
+  '       bootstrap the installation of "pip" (using the command: "pip install pip").\n'
+  '       Running "pip" from a remote shared drive may be slow (~30s to install pip),\n'
+  '       so it should be avoided once "pip" is installed locally.\n'
+  'clone: This command copies a list of files/directories from the "cwd" (current\n'
+  '       working directory) to "site-packages(user)" (PIP_TARGET), or to a directory\n'
+  '       with a file "CLONE_TARGET.py" (the directories have to be on the "sys.path"\n'
+  '       search path so Python can find them).\n'
+  '       Tip 1: Copy/paste the filenames from the "ls" output to the input line.\n'
+  '              (Line breaks in the pasted text are not a problem.)\n'
+  '       Tip 2: Copy "pip_sh.py" to a remote shared drive, and run it from there.\n'
+  '              (The initial "cwd" is the parent directory of the script being run.)\n'
+  '   ls: List an alphabetically sorted list of files/directories of the "cwd".\n' 
+  '   cd: Change the "cwd".\n'
+ f" exit: Exit '{sh_name}'.\n"
+  ' help: This help text.\n'
+  ) 
 try:
   import PIP_TARGET
 except ImportError:
@@ -18,27 +39,27 @@ try:
 except ImportError: pass
 
 def clone_selected(src, dst, selected, logging=False):
-    src, dst = pathlib.Path(src), pathlib.Path(dst)
-    if logging: print(f'\nCopying {len(selected)} items:') 
-    for i,item in enumerate(selected):
-      if logging: print(f'{i+1:5d}: {item:30}',end='')
-      try:
-        src_path = src / item
-        dst_path = dst / item
-        if os.path.exists(dst_path):
-          if logging: print("can't overwrite existing item",file=sys.stderr)
-        elif src_path.is_dir():
-          shutil.copytree(src_path, dst_path,copy_function=shutil.copy)
-          if logging: print("O.K.")
-        elif src_path.is_file():
-          shutil.copy(src_path, dst_path)
-          if logging: print("O.K.")
-        else:
-          if logging: print("source item invalid",file=sys.stderr)
-      except Exception as e:
-        if logging:
-          print()
-          print(e,file=sys.stderr)
+  src, dst = pathlib.Path(src), pathlib.Path(dst)
+  if logging: print(f'\nCopying {len(selected)} items:') 
+  for i,item in enumerate(selected):
+    if logging: print(f'{i+1:5d}: {item:30}',end='')
+    try:
+      src_path = src / item
+      dst_path = dst / item
+      if os.path.exists(dst_path):
+        if logging: print("can't overwrite existing item",file=sys.stderr)
+      elif src_path.is_dir():
+        shutil.copytree(src_path, dst_path,copy_function=shutil.copy)
+        if logging: print("O.K.")
+      elif src_path.is_file():
+        shutil.copy(src_path, dst_path)
+        if logging: print("O.K.")
+      else:
+        if logging: print("source item invalid",file=sys.stderr)
+    except Exception as e:
+      if logging:
+        print()
+        print(e,file=sys.stderr)
 
 def sh_cmd(cmdln):
   if not cmdln:
@@ -48,46 +69,24 @@ def sh_cmd(cmdln):
   cmd,*args=sys.argv
   try:
     if   'clone'==cmd: clone_selected(src=os.getcwd(),dst=cloned_packages_path,selected=args,logging=True);return 0
-    elif 'ls'   ==cmd: print('\n',' \n '.join(map(shlex.quote,sorted(os.listdir(*args)))),' ');  return 0
+    elif 'ls'   ==cmd: print('\n',' \n '.join(map(shlex.quote,sorted(os.listdir(*args)))),'\n');  return 0
     elif 'cd'   ==cmd: os.chdir(*args); return 0
+    elif 'help' ==cmd: print(help_text(),end='\n\n')
     elif 'quit' ==cmd: sys.exit()
     elif 'exit' ==cmd: sys.exit()
     else:              
-                       try:
-                         runpy.run_module(cmd,run_name='__main__')
-                       except Exception as e:
-                         raise e
-                       except BaseException:
-                         return 0
+      try: runpy.run_module(cmd,run_name='__main__')
+      except Exception as e: raise e
+      except BaseException: return 0
   finally:
     sys.argv=saved_argv
-
+    
 def sh():
-  print(
-  'Command List:\n'
-  '  pip: This is the standard Python pip command.\n'
-  '       If "pip" is not yet installed, copy "pip_sh.py" to a remote share that\n'
-  '       contains an installed "pip" module (e.g. on a shared remote drive) to\n'
-  '       bootstrap the installation of "pip" (using the command: "pip install pip").\n'
-  '       Running "pip" from a remote shared drive may be slow (~30s to install pip),\n'
-  '       so it should be avoided once "pip" is installed locally.\n'
-  'clone: This command copies a list of files/directories from the "cwd" (current\n'
-  '       working directory) to "site-packages(user)" (PIP_TARGET), or to a directory\n'
-  '       with a file "CLONE_TARGET.py" (the directories have to be on the "sys.path"\n'
-  '       search path so Python can find them).\n'
-  '       Tip 1: Copy/paste the filenames from the "ls" output to the input line.\n'
-  '              (Line breaks in the pasted text are not a problem.)\n'
-  '       Tip 2: Copy "pip_sh.py" to a remote shared drive, and run it from there.\n'
-  '              (The initial "cwd" is the parent directory of the script being run.)\n'
-  '   ls: List an alphabetically sorted list of files/directories of the "cwd".\n' 
-  '   cd: Change the "cwd".\n'
-  ' exit: exit "pip_sh".\n'
-  )
-  
+  print(help_text())
   while True:
     try:
       cwd=os.path.basename(os.getcwd())
-      cmdln=input(f'\npip_sh:{cwd} $ ')
+      cmdln=input(f'{sh_name}:{cwd} $ ')
       sh_cmd(cmdln)
     except Exception as e:
       print(e,file=sys.stderr)
@@ -95,5 +94,7 @@ def sh():
       break 
       
 if __name__=='__main__':
+  if len(sys.argv)>1: sh_name=sys.argv[1]
   sh()
+  print(f"\nKeyboard interrupt received, exiting '{sh_name}'. ")
 
