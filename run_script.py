bad3c5a8cf7a004e758d549394879287922fde5a
Richard Potthoff <RichardPotthoff@users.noreply.github.com>
2026-08-21 12:32:10 -0400

    update run_script.py: add script's home directory to sys.path

--- run_script.py
+++ run_script.py
@@ -1,10 +1,17 @@
-import sys, runpy
+import os, sys, runpy
 saved_argv=sys.argv
+saved_path=sys.path
 try:
   script_path=sys.argv[1]
-  sys.argv=sys.argv[1:]
+  #print(f'run_script(original): {sys.argv=}')
+  sys.argv=sys.argv[1:-1]#there seems to be an extra argument appended at the end when called from pythonista tools
+  sys.path=[os.path.dirname(script_path)]+saved_path
+  #print(f'run_script(shifted): {sys.argv=}')
   print("'run_script.py': Executing " '"' f"runpy.run_path('{script_path }',run_name='__main__')" '".')
   runpy.run_path(script_path,run_name='__main__')
   print( "'run_script.py': Finished!")
 except Exception as e: print(e,file=sys.stderr)
-finally: sys.argv=saved_argv
+finally: 
+  sys.argv=saved_argv
+  sys.path=saved_path
+  
